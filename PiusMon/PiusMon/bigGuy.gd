ce776451418f1a62ee6d3d2830729a752d444d84
Angelo Banacle <106713762+Tone-deaf@users.noreply.github.com>
2023-06-02 03:39:52 +1000

    Add files via upload

--- PiusMon/PiusMon/bigGuy.gd
+++ PiusMon/PiusMon/bigGuy.gd
@@ -2,5 +2,17 @@ extends Node2D
 # Creating stats for character
 @export var is_player : bool
 @export var maxHealth : int = 40
-@export var dammage : int = 20
-@export var speed : int = 40
+@export var damage : int = 30
+@export var speed : int = 20
+
+
+# Updating healthbar whenever it changes
+func _ready() -> void:
+  update_health_ui()
+  $healthBar.max_value = maxHealth
+
+func update_health_ui():
+  set_health_bar()
+
+func set_health_bar() -> void:
+  $healthBar.value = maxHealth
---
+++ PiusMon/PiusMon/thorny.gd
@@ -0,0 +1,6 @@
+extends Node2D
+# Creating stats for character
+@export var is_player : bool
+@export var maxHealth : int = 30
+@export var damage : int = 30
+@export var speed : int = 40
